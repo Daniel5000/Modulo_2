530dbb3f043af44b88ff7c5ea23265dd1f7e984a
dacasti5000 <dacasti5000@gmail.com>
2016-12-26 16:03:58 -0500

    cambios realizados lista

--- app.js
+++ app.js
@@ -2,83 +2,133 @@
 'use strict';
 
 angular.module('ShoppingListApp', [])
-.controller('ShoppingListController', ShoppingListController)
+.controller('ShoppingListController', ShoppingListController,'$scope')
+.controller('BoughtListController',BoughtListController)
 .provider('ShoppingListService', ShoppingListServiceProvider)
 .config(Config);
 
-Config.$inject = ['ShoppingListServiceProvider'];
-function Config(ShoppingListServiceProvider) {
-  // Save Yaakov from himself
-  ShoppingListServiceProvider.defaults.maxItems = 2;
+
+
+Config.$inject=["ShoppingListServiceProvider"]
+function Config(ShoppingListServiceProvider)
+{
+    ShoppingListServiceProvider.defaults.maxItems=5;
 }
 
+ShoppingListController.$inject=['ShoppingListService']
+function ShoppingListController(ShoppingListService)
+{
+  var list=this;
+  list.items=ShoppingListService.getItems();
 
-ShoppingListController.$inject = ['ShoppingListService'];
-function ShoppingListController(ShoppingListService) {
-  var list = this;
+  list.itemName="";
+  list.itemQuantity="";
 
-  list.items = ShoppingListService.getItems();
 
-  list.itemName = "";
-  list.itemQuantity = "";
-
-  list.addItem = function () {
+  list.boughtItem=function(itemName,itemQuantity,itemIndex)
+  {
     try {
-      ShoppingListService.addItem(list.itemName, list.itemQuantity);
+    ShoppingListService.boughtItem(itemName,itemQuantity,itemIndex);
+    list.removeItem(itemIndex);
+
     } catch (error) {
-      list.errorMessage = error.message;
+      list.errorMessage=error.message;
     }
   };
 
-  list.removeItem = function (itemIndex) {
+  list.addItem=function(itemName,itemQuantity)
+  {
+    try {
+      ShoppingListService.addItem(itemName,itemQuantity);
+
+    } catch (error) {
+      list.errorMessage=error.message;
+    }
+  };
+
+  list.removeItem=function(itemIndex)
+  {
     ShoppingListService.removeItem(itemIndex);
   };
 }
 
+function ShoppingListService(maxItems)
+{
+  var service=this;
 
-// If not specified, maxItems assumed unlimited
-function ShoppingListService(maxItems) {
-  var service = this;
+  var items=[ { name: "cookies", quantity: 10 },
+              { name: "Coffee",  quantity: 10 },
+              { name: "Glasses", quantity: 10 },
+              { name: "Apples",  quantity: 10 },
+              { name: "Melons",  quantity: 10 },
+            ];
 
-  // List of shopping items
-  var items = [];
+  var boughtItems=[];
 
-  service.addItem = function (itemName, quantity) {
-    if ((maxItems === undefined) ||
-        (maxItems !== undefined) && (items.length < maxItems)) {
-      var item = {
-        name: itemName,
-        quantity: quantity
+
+
+service.removeItem=function(itemIndex)
+{
+  items.splice(itemIndex,1);
+};
+
+service.getItems=function()
+{
+  return items;
+};
+
+  service.boughtGetItems=function()
+  {
+    return boughtItems;
+  }
+
+  service.boughtItem=function(itemName, quantity)
+  {
+    if ((maxItems===undefined)|| (maxItems!==undefined)&&(boughtItems.length<maxItems))
+    {
+      var item={
+        name:itemName,
+        quantity:quantity
       };
-      items.push(item);
+      boughtItems.push(item);
     }
     else {
-      throw new Error("Max items (" + maxItems + ") reached.");
+      throw new Error("Max items (" + maxItems +")reached.");
     }
   };
 
-  service.removeItem = function (itemIndex) {
-    items.splice(itemIndex, 1);
-  };
+}
 
-  service.getItems = function () {
-    return items;
+BoughtListController.$inject=['ShoppingListService']
+function BoughtListController(ShoppingListService)
+{
+  var list=this;
+  list.items=ShoppingListService.boughtGetItems();
+
+  list.itemName="";
+  list.itemQuantity="";
+
+  list.removeItem=function(itemIndex)
+  {
+    BoughtListService.removeItem(itemIndex);
   };
 }
 
 
-function ShoppingListServiceProvider() {
-  var provider = this;
+function ShoppingListServiceProvider()
+{
+  var provider=this;
 
-  provider.defaults = {
-    maxItems: 10
+  provider.defaults={
+    maxItems:10
   };
 
-  provider.$get = function () {
-    var shoppingList = new ShoppingListService(provider.defaults.maxItems);
-
+  provider.$get=function()
+  {
+    var shoppingList=new ShoppingListService(provider.defaults.maxItems);
     return shoppingList;
   };
+
 }
 
 })();
